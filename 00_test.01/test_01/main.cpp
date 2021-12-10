1098fef4f6a242abf666a26b3c9a26f85b37e678
Piotr Heinzelman <piotr.heinzelman@vp.pl>
2021-12-10 23:58:30 +0100

    test_01 :: get bigger / equals int

--- 00_test.01/test_01/main.cpp
+++ 00_test.01/test_01/main.cpp
@@ -2,30 +2,59 @@
 
 using namespace std;
 
-int getBigger( int first, int second ){
-    if ( first > second ) return first;
-    return second;
+
+/*
+
+Wczytaæ dwie liczby ca³kowite i wydrukowaæ wiêksz¹ z nich.
+Uwzglêdniæ przypadek, gdy dwie liczby s¹ sobie równe i odpowiednio sygnalizowaæ
+to komentarzem.
+
+only if
+
+*/
+
+/*
+
+https://www.p-programowanie.pl/cpp/polaczenie-assemblera-i-cpp
+
+void funkcja1() {
+    __asm("mov eax, 5");
+    __asm("push eax");
+    __asm("add esp, 4");
 }
 
+void funkcja2() {
+    asm("\
+        mov eax, 5 \n\
+        push eax \n\
+        add esp, 4 \n\
+    ");
+}
+
+*/
 
 
 int main(){
-    int first, second, bigger;
-    cout << "Podaj dwie liczby: " << endl;
-    cin >> first >> second ;
 
-    cout << "First: " << first << endl;
-    cout << "Second: " << second << endl;
+    int first, second;
 
-    bigger = getBigger( first, second );
+    cout << "Podaj dwie liczby: " << (char)0x0a <<  (char)0x0d;
+    cout << "Liczba pierwsza: ";
 
-    cout << "Wiêksza liczba: " <<  bigger << endl;
+    cin >> first;
+
+    cout << "Liczba druga: ";
+
+    cin >> second;
+
+    if ( first == second ) { cout << "Liczby s¹ jednakowe"<< (char)0x0a << (char)0x0d; return 1; }
+    if ( first > second )  { cout << "Liczba pierwsza jest wiêksza. (" << first << ")" << (char)0x0a << (char)0x0d; return 0;}
+    cout << "Liczba druga jest wiêksza. (" << second << ")" << (char)0x0a << (char)0x0d;
+
 
     system("pause");
     return 0;
+
 }
-
-
-
 
 
